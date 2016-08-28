b50cd92bcb0d5bcaa47e224194a865a028920431
kazuhira <kazuhira.m@gmail.com>
2016-08-28 23:12:25 +0900

    reflect, latest artist

--- src/main/scala/org/littlewings/lyricsbot/servlet/ScheduledLyricsBotServlet.scala
+++ src/main/scala/org/littlewings/lyricsbot/servlet/ScheduledLyricsBotServlet.scala
@@ -8,8 +8,8 @@ import org.littlewings.lyricsbot.{ScheduledLyricsBot, artist}
 class ScheduledLyricsBotServlet extends HttpServlet {
   private val scheduledLyricsBot: ScheduledLyricsBot =
   //artist.ScheduledConsoleLilyMyuBot
-  //artist.ScheduledTwitterLilyMyuBot
-    artist.ScheduledTwitterCaratBot
+  // artist.ScheduledTwitterLilyMyuBot
+  artist.ScheduledTwitterCaratBot
 
   override def init(): Unit = {
     scheduledLyricsBot.startJob()
